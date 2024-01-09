59a4239d009e9fa220e5990e38a105504e7d8897
Yury.Chychayeu <chichaevwork@gmail.com>
2024-01-09 10:33:59 +0100

    TestNG features

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Calculator {
+
+    public int div(int a, int b){
+        return a/b;
+    }
+
+    public double div(double a, double b){
+        return a/b;
+    }
+}
